dc04a2ff96adab84d3d146828595d9b11dc54dcf
Bhaskar Anand <85471852+BhaskarAnand-05@users.noreply.github.com>
2022-01-21 13:45:28 +0530

    New commit

--- FCFS.cpp
+++ FCFS.cpp
@@ -7,7 +7,7 @@ int main()
     system("cls");
     int process[100];
     int n;
-    int burst_time[100], waiting_time[100], Turn_Around_time[100];
+    int burst_time[100], waiting_time[100], completion_time[100];
 
     cout << endl
          << "Enter the No. of Processes to be executed through CPU" << endl
@@ -23,20 +23,20 @@ int main()
         cout << endl;
     }
     // Calculating the Turn_Around Time of the processes
-    Turn_Around_time[0] = 0;
+    completion_time [-1] = 0;
 
     for (int i = 0; i < n; i++)
     {
-        Turn_Around_time[i] = Turn_Around_time[i - 1] + burst_time[i];
+        completion_time[i] = completion_time[i - 1] + burst_time[i];
     }
     // Calculating the Waiting Time of the processes
     waiting_time[0] = 0;
     for (int i = 1; i < n; i++)
     {
-        waiting_time[i] = Turn_Around_time[i - 1];
+        waiting_time[i] = completion_time[i - 1];
     }
 
-    // Displaying the Ouput of the Processes
+    // Displaying the (Ouput) Average Waiting Time of the Processes
     system("cls");
     cout << "\t\t\t\t First- Come, First-Served (FCFS) Scheduling" << endl
          << endl;
@@ -51,8 +51,28 @@ int main()
 
     for (int i = 0; i < n; i++)
     {
-        cout << "Process [" << i + 1 << "]\t\t\t\t" << burst_time[i] << "\t\t\t\t\t\t  " << Turn_Around_time[i] << "\t\t\t\t\t\t" << waiting_time[i] << endl;
+        cout << "Process [" << i + 1 << "]\t\t\t\t" << burst_time[i] << "\t\t\t\t\t\t  " << completion_time[i] << "\t\t\t\t\t\t" << waiting_time[i] << endl;
     }
+    // Calculating Average Waiting Time For the Problem
+    float Average_Waiting_Time = 0;
+
+    cout << "\n\nAverage Waiting Time = \t"
+         << "Sum of Waiting time for all Processes / Total No. of Process" << endl<<endl<<endl;
+
+    cout << "Average Waiting Time = \t [" ;
+    for (int i = 0; i < n - 1; i++)
+    {
+        cout << waiting_time[i] << " + ";
+    }
+    cout<< waiting_time[n-1] << "] /"<<n;
+
+    for (int i = 0; i < n; i++)
+    {
+        Average_Waiting_Time += waiting_time[i];
+    }
+
+    cout << endl
+         << "Average Waiting Time = \t" << Average_Waiting_Time/n;
 
     return 0;
 }
