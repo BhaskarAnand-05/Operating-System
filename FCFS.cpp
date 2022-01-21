75c6513c93dfb4a1d9dc2b4879882ab5d0fc421b
Bhaskar Anand <85471852+BhaskarAnand-05@users.noreply.github.com>
2022-01-21 11:48:19 +0530

    Completion to Turnaround

--- FCFS.cpp
+++ FCFS.cpp
@@ -7,7 +7,7 @@ int main()
     system("cls");
     int process[100];
     int n;
-    int burst_time[100], waiting_time[100], completion_time[100];
+    int burst_time[100], waiting_time[100], Turn_Around_time[100];
 
     cout << endl
          << "Enter the No. of Processes to be executed through CPU" << endl
@@ -22,18 +22,18 @@ int main()
         cin >> burst_time[i];
         cout << endl;
     }
-    // Calculating the Completion Time of the processes
-    completion_time[0] = 0;
+    // Calculating the Turn_Around Time of the processes
+    Turn_Around_time[0] = 0;
 
     for (int i = 0; i < n; i++)
     {
-        completion_time[i] = completion_time[i - 1] + burst_time[i];
+        Turn_Around_time[i] = Turn_Around_time[i - 1] + burst_time[i];
     }
     // Calculating the Waiting Time of the processes
     waiting_time[0] = 0;
     for (int i = 1; i < n; i++)
     {
-        waiting_time[i] = completion_time[i - 1];
+        waiting_time[i] = Turn_Around_time[i - 1];
     }
 
     // Displaying the Ouput of the Processes
@@ -45,13 +45,13 @@ int main()
          << "\t\t\t\t\t"
          << "Burst Time"
          << "\t\t\t\t\t"
-         << "Completion Time"
+         << "Turnaround Time"
          << "\t\t\t\t\t"
          << "Waiting Time" << endl;
 
     for (int i = 0; i < n; i++)
     {
-        cout << "Process [" << i + 1 << "]\t\t\t\t" << burst_time[i] << "\t\t\t\t\t\t  " << completion_time[i] << "\t\t\t\t\t\t" << waiting_time[i] << endl;
+        cout << "Process [" << i + 1 << "]\t\t\t\t" << burst_time[i] << "\t\t\t\t\t\t  " << Turn_Around_time[i] << "\t\t\t\t\t\t" << waiting_time[i] << endl;
     }
 
     return 0;
